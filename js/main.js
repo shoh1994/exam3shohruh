0fc6bea54e58a9bbd0aa657e34103d578f38dff0
shohrux <nurmatovshohruhbek1@gmail.com>
2023-05-27 17:41:26 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+window.addEventListener("scroll", function () {
+    toggleBacktop();
+  });
+  
+  let backtop = document.getElementById("backtop");
+  
+  function toggleBacktop() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      backtop.style.bottom = "50px";
+    } else {
+      backtop.style.bottom = "-50px";
+    }
+  }
+  const loading = document.getElementById("loading");
+setTimeout(() => {
+  loading.classList.add('loading-none');
+}, 3000);
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+		navbar.classList.remove("navbar-shrink");
+	}
+}
